f74707a77f7fe884cb807a1d18761051fc6a5a4f
mikeli82 <77188730+mikeli82@users.noreply.github.com>
2022-03-15 11:36:30 +0800

    Initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mike Li on 2022/3/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var deduct: UIButton!
+    @IBOutlet weak var equals: UIButton!
+    
+    @IBAction func anyNumberTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func anyOperatorTapped(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
